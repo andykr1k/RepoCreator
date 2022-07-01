d07757d4cdc071006b68e6a5760a49f627494f6a
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-07-01 14:21:56 -0700

    Repo Generator Works...Needs to implement Prompt Control

--- main.cpp
+++ main.cpp
@@ -8,34 +8,34 @@
 #include<unistd.h>
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    string name = argv[1];
-    // Clear Terminal Window
+int main() {
+    string name;
+
     system("clear");
 
-    // Change Directory to Desktop/Projects
-    system("cd /Users/andrew/Desktop/Projects/ && mkdir test && cd test && ls");
+    cout << "What would you like to name your repo?" << endl;
+    cin >> name;
+    
+    //Create command string
+    string command = "cd /Users/andrew/Desktop/Projects/ && mkdir " + name + " && cd " + name +" && git init && gh repo create";
+
+    //Getting length for char array
+    int n = command.length();
+ 
+    // declaring character array
+    char char_array[n + 1];
+ 
+    // copying the contents of the command string to char array
+    strcpy(char_array, command.c_str());
+
+    /* TESTING
+    for (int i = 0; i < n; i++)
+        cout << char_array[i];
+    */
+
+    // Run Command
+    system(char_array);
 }
-
-//Steps to creating a repo
-
-//Change to root - Done
-//cd ~
-
-//Change to Desktop Projects folder - Done
-//cd Desktop/Projects
-
-//Make new directory - Done
-//mkdir [name]
-
-//Move to directory - Done
-//cd [name]
-
-//Initialize git init - Done
-//git init
-
-//Create repo using gh cli
-//gh repo create
 
 //push to git
 //2nd option
